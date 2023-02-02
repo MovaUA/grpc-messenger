5dfb8f4e3bae4b69a37ef6eed1782c5f55351214
Valeriy <valeriy.molchanov.77@gmail.com>
2023-02-02 13:04:17 +0200

    configure http2 logging (#8)

--- run-server.sh
+++ run-server.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
+export GODEBUG=http2debug=2
 export GRPC_GO_LOG_VERBOSITY_LEVEL=99
 export GRPC_GO_LOG_SEVERITY_LEVEL=info
 
